045086c398018b450d09dbf3d0d5b50a10a7685d
DevTierspace <165806291+tiersp4ce@users.noreply.github.com>
2024-09-17 17:42:16 -0300

    Create Ingresso.java

---
+++ 3º Periodo/POO/Trabalho 1/Q2/Ingresso.java
@@ -0,0 +1,29 @@
+package Q2;
+
+public class Ingresso {
+
+double valor;
+
+//Construtor
+
+public Ingresso(double valor){
+this.valor = valor;
+}
+
+//Get e Set
+
+public double getvalor(){
+    return valor;
+}
+public void setvalor(double valor){
+    this.valor = valor;
+}
+
+public String imprimeValor(){
+    
+    
+    return null;
+}
+
+
+}
